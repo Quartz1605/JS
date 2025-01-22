a7d6a46547d1c73b6ddf8749f0ffed736e37f48c
Pratham <prathambarekal05@gmail.com>
2025-01-22 09:10:50 +0000

    NotesAdded

---
+++ 01_basics/notes.js
@@ -0,0 +1,13 @@
+/* Primitive : 7
+String,Number,Boolean,null(empty),undefined,Symbol,BigInt.*/
+
+/* Referencetype(Non-primitive)
+Arrays,Objects,Functions.*/
+
+//Javascript is dynamically typed language.
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+//dono alag honge,bhale hi aap same value pass karo.
+
+
