ac3dbdc5b5ed4a98df35de2a7e0bf3e56284ab49
Pratham <prathambarekal05@gmail.com>
2025-01-22 09:47:20 +0000

    Added Memory

--- 01_basics/notes.js
+++ 01_basics/notes.js
@@ -11,3 +11,37 @@ const anotherId = Symbol('123');
 //dono alag honge,bhale hi aap same value pass karo.
 
 
+// Stack(Primitive) and Heap(Non-Primitive) Memory.
+
+let myYoutubeName = "pratham.com"
+let yourYoutube = myYoutubeName
+
+yourYoutube = "raj.com";
+
+console.log(yourYoutube);
+console.log(myYoutubeName);
+
+//Primitive datatype mein copy pass on hoti hai,so original remains the same without any changes.
+
+//But in Heap
+
+let userOne = {
+    email:"pratham.com",
+    upi : "pratham@okhdcf",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "pratham@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+//Isme userone mein bhi change ho jaata cus call  by refernce hota hai and
+//uske wajah se copy nahi jaati.
+
+
+
+
+
+
