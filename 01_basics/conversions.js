f219db967fc9cc1d3c29a88da7bea3339ad4acab
Pratham <prathambarekal05@gmail.com>
2025-01-22 07:37:43 +0000

    conversionsAndOperations

---
+++ 01_basics/conversions.js
@@ -0,0 +1,36 @@
+let score = "33abc"
+
+
+
+
+let valueInNumber = Number(score);
+//Use Capital "Number" not "number", same with string to convert. 
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//Convert ho jayega but jab print karoge tab dikkat dega aapko.
+
+let score1 = null;
+let valueInNumber1 = Number(score1);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+//boolean mein 1 and 0 mein convert ho jayega.
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// "" => false
+//"pratham" => true.
+
+let sum = 33;
+let strNum =  Sting(sum)
+console.log(typeof strNum)
+
+
---
+++ 01_basics/operations.js
@@ -0,0 +1,20 @@
+let value =  3;
+let negValue = -value;
+//console.log(negValue);
+
+//console.log(2**4);
+//console.log(13%3);
+
+let str1 = "hello"
+let str2 = " pratham"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+console.log("1" + 2 + 2)
+/*but*/
+console.log(1 + 2 + "3")
+
+/*agar pehle string hai toh no math operations code will act like string.
+But if later string hai toh pehle math opertaions then string operations.*/
+
