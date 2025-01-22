eebb28021617474dd23d7041608b44ad9529bf21
Pratham <prathambarekal05@gmail.com>
2025-01-22 06:50:17 +0000

    Learnt datatypes.

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello");  
+/* JS ka engine is chhupa hua in Browser 
+ We use node.js to run javascript locally.
+ So ye code nahi execute hoga,cus hum code execute nahi karre in browser.*/
+
+console.log(3+2);
+console.log("Pratham.");
+//Code readability should be high.
+
+//"ECMA" defines how code is executed in Javascript.
+//tc39 and MDN hum isse documentation refer karenge.
+
+
+let pratham = "Pratham"
+
+let age = 18
+let isLoggedIn = true
+let temp = null;
+
+/* Datatypes in Javascript.
+// number => 2 to power of of 53 ye range hai number ki.
+// string => ""
+// boolean => true/false
+// null => standalone value.
+// undefined => kuch assign hi nahi hua hai.
+// symbol => unique */
+
+//object
+
+console.log(typeof pratham)
+
+//null ki type of value "object" aati hai.
+//undefined is typeof value "undefined" hi aati hai.
+
+
+
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+let accountEmail = "hitesh@gooogle.com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+let accountState;
+
+//accountId = 2
+//const can't can be changed after that.
+
+accountEmail = "hcfc@hgmail.com"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword]);
+
+//Never use "var".Because of issue in block scope and functional scope.
+
