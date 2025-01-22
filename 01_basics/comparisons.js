ab957ac6b85bba3c25a465e0297435d9acdafb04
Pratham <prathambarekal05@gmail.com>
2025-01-22 08:35:28 +0000

    comparisons

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,16 @@
+console.log(1 > 2)
+
+console.log("2" > 1)
+/*code chalega but mat karna,always compare two same datatypes.*/
+
+console.log(null > 0);
+console.log(null >= 0);
+
+/*comparisons convert null to a number,treating it as 0.
+but null == 0 bhi false aayega aapko , "==" thoda alag chalta hai*/
+
+console.log(undefined == 0); //Yeh harr ek ke liye false dega.
+
+console.log("2" === 2);
+//"===" strict checking karta hai.
+
