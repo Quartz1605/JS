0790c73a6e097d112b973a9fd132305a973f4ba4
Pratham <prathambarekal05@gmail.com>
2025-01-23 19:43:46 +0000

    rest operator in functions.

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,41 @@
+function cartPrice(num1){
+    return num1;
+}
+
+//console.log(cartPrice(100));
+//But agar hum do teen pass kiye argument toh bass first value arg return hoti hai.
+
+//Rest operator 
+function cartPrice2(...num1){
+    return num1;
+}
+
+console.log(cartPrice2(100,400,542,123));
+//Abhi aapke pass array return hota hai,which helps aapko aage bhi,"spread" operator jaisa hi syntax hai iska.
+
+function cartPrice3(val1,val2,...num1){
+    return num1;
+}
+//Iss case mein first two arguments will treated as normal arguments and uske baad walo ka array conversion hoga.
+
+//Objects in Functions.
+const user = {
+    username : "Pratham",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username},and your price is ${anyobject.price}`)
+}
+
+//Arrays in Function.
+
+const randomArray = ["pratham","pranav","ahmed"]
+
+function friends(randomArray){
+    for(let i=0;i<3;i++){
+        console.log(`Name of the ${i+1} friend is ${randomArray[i]}`);
+    }
+}
+
+friends(randomArray)
