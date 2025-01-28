333fd41b435fb106b5f5844330fea9672495c2b8
Pratham <prathambarekal05@gmail.com>
2025-01-28 05:06:05 +0000

    Learnt async functions like setTimeInterval, setTimeout.

--- projects/03_Clock.js
+++ projects/03_Clock.js
@@ -2,6 +2,9 @@
 
 ///Iska structure kaafi important hai.Log bohot galti hai isme...
 
+//Basically ek function hai,jo given time period ke baad khudko repeat karega...(Time is in milliseconds.)\
+//Itne time ke baad mein aapne aap ko repeat karunga.
+
 setInterval(() => {
     let date2 = new Date()
 
